ec82aebc65ab02136e44e34f04edc954998c5ada
Victor LI <victor-li@outlook.com>
2025-02-16 22:29:12 +0800

    Enhance localization support in strings.lua by adding Traditional Chinese translations and updating key formatting

--- strings.lua
+++ strings.lua
@@ -1,12 +1,17 @@
 function data()
     return {
         en = {
-            MOD_NAME = "Train Operation Diagram",
-            MOD_DESC = "A diagram to show the operation of trains in the game.",
+            ["MOD_NAME"] = "Train Operation Diagram",
+            ["MOD_DESC"] = "A diagram to show the operation of trains in the game.",
         },
-        zh = {
-            MOD_NAME = "列车运行图",
-            MOD_DESC = "显示游戏中列车的运行图。",
+        zh_CN = {
+            ["MOD_NAME"] = "列车运行图",
+            ["MOD_DESC"] = "显示游戏中列车的运行图。",
+        },
+        zh_TW = {
+            ["MOD_NAME"] = "列車運行圖",
+            ["MOD_DESC"] = "顯示遊戲中列車的運行圖。",
         }
+
     }
 end
